85ff7480cd7977b7b5fe5ac713240274955aabd8
Fedor Zanin <ZaninF@danieli.it>
2021-11-05 10:06:27 -0400

    initalizations for Module 09

---
+++ Module_08/Homework_09_Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_09_Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
