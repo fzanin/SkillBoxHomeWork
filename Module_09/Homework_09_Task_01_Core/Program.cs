24dab6fbc8ed0b2de99429743ff3fc037b27bf65
Fedor Zanin <ZaninF@danieli.it>
2021-11-05 15:26:45 -0400

    add 09 Core project template

---
+++ Module_09/Homework_09_Task_01_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_09_Task_01_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
