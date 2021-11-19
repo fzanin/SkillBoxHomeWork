1f5a5107e1593ce7012f30113b711dd064511867
Fedor Zanin <ZaninF@danieli.it>
2021-11-18 19:40:40 -0500

    bot's logic moved to its  own class

---
+++ Module_09/Homework_09_Task_01/LogHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_09_Task_01
+{
+    public class LogHelper
+    {
+
+        public void Logging(string logMessage)
+        {
+            Console.WriteLine($"{DateTime.Now} - {logMessage}");
+        }
+
+    }
+}
--- Module_09/Homework_09_Task_01/Program.cs
+++ Module_09/Homework_09_Task_01/Program.cs
@@ -18,28 +18,35 @@ namespace Homework_09_Task_01
     class Program
     {
 
-        static TelegramBotClient tbClient;
+
+        //////static TelegramBotClient tbClient;
 
         [Obsolete]
         static void Main(string[] args)
         {
 
-            tbClient = new TelegramBotClient("2063385048:AAExMggPQfFvRov0ckLf3y1hdickAci6Zlw");
-
-            tbClient.OnMessage += BotOnMessageReceiver;
-            tbClient.OnCallbackQuery += TbClient_OnCallbackQueryReceiver;
-
-
-            var tbMe = tbClient.GetMeAsync().Result;
-
-            Console.WriteLine(tbMe.FirstName);
-
-            tbClient.StartReceiving();
-
-
+            BotController botController = new BotController("2063385048:AAExMggPQfFvRov0ckLf3y1hdickAci6Zlw");
             Console.ReadLine();
 
-            tbClient.StopReceiving();
+
+
+
+            //////tbClient = new TelegramBotClient("2063385048:AAExMggPQfFvRov0ckLf3y1hdickAci6Zlw");
+
+            //////tbClient.OnMessage += BotOnMessageReceiver;
+            //////tbClient.OnCallbackQuery += TbClient_OnCallbackQueryReceiver;
+
+
+            //////var tbMe = tbClient.GetMeAsync().Result;
+
+            //////Console.WriteLine(tbMe.FirstName);
+
+            //////tbClient.StartReceiving();
+
+
+            //////Console.ReadLine();
+
+            //////tbClient.StopReceiving();
 
 
             //WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
@@ -96,92 +103,92 @@ namespace Homework_09_Task_01
 
         }
 
-        [Obsolete]
-        private static async void TbClient_OnCallbackQueryReceiver(object sender, /*Telegram.Bot.Args.*/CallbackQueryEventArgs e)
-        {
-            string buttonText = e.CallbackQuery.Data;
-            string name = $"{e.CallbackQuery.From.FirstName} {e.CallbackQuery.From.LastName}";
-            Console.WriteLine($"{name} press command: {buttonText}");
+//////        [Obsolete]
+//////        private static async void TbClient_OnCallbackQueryReceiver(object sender, /*Telegram.Bot.Args.*/CallbackQueryEventArgs e)
+//////        {
+//////            string buttonText = e.CallbackQuery.Data;
+//////            string name = $"{e.CallbackQuery.From.FirstName} {e.CallbackQuery.From.LastName}";
+//////            Console.WriteLine($"{name} press command: {buttonText}");
 
-            await tbClient.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"You press command {buttonText}");
-
-
-
-        }
-
-        [Obsolete]
-        private static async void BotOnMessageReceiver(object sender, MessageEventArgs e)
-        {
-            var message = e.Message;
-
-            if (message == null || message.Type != MessageType.Text)
-                return;
-
-
-            string name = $"{message.From.FirstName} {message.From.LastName}";
-
-            Console.WriteLine($"{name} sent message: {message.Text}");
-
-            switch (message.Text)
-            {
-                case "/start":
-                    string text = @"List of commands: 
-                                    /start    - bot start 
-                                    /menu     - show menu
-                                    /keyboard - show keyboard";
-                    await tbClient.SendTextMessageAsync(message.From.Id, text);
-
-                    break;
-
-                case "/menu":
-
-                    var inLineKeyboard = new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithUrl("Google", "https://google.com"),
-                            InlineKeyboardButton.WithUrl("Telegram", "https://t.me")
-                        },
-                        new []
-                        {
-                            InlineKeyboardButton.WithCallbackData("Item #1"),
-                            InlineKeyboardButton.WithCallbackData("Item #2")
-
-                        }
-                    });
-
-                    await tbClient.SendTextMessageAsync(message.From.Id, "Select", replyMarkup: inLineKeyboard);
-
-
-                    break;
-                case "/keyboard":
-
-                    var replyKeyboard = new ReplyKeyboardMarkup(new[]
-{
-                        new[]
-                        {
-                            new KeyboardButton("Hi"),
-                            new KeyboardButton("How are you?")
-                        },
-                        new []
-                        {
-                            new KeyboardButton("Contact") {RequestContact = true},
-                            new KeyboardButton("Location") {RequestLocation = true}
-
-                        }
-                    });
-
-                    await tbClient.SendTextMessageAsync(message.Chat.Id, "Message", replyMarkup: replyKeyboard);
-
-                    break;
-
-                default:
-                    break;
-            }
+//////            await tbClient.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"You press command {buttonText}");
 
 
 
+//////        }
 
-        }
+//////        [Obsolete]
+//////        private static async void BotOnMessageReceiver(object sender, MessageEventArgs e)
+//////        {
+//////            var message = e.Message;
+
+//////            if (message == null || message.Type != MessageType.Text)
+//////                return;
+
+
+//////            string name = $"{message.From.FirstName} {message.From.LastName}";
+
+//////            Console.WriteLine($"{name} sent message: {message.Text}");
+
+//////            switch (message.Text)
+//////            {
+//////                case "/start":
+//////                    string text = @"List of commands: 
+//////                                    /start    - bot start 
+//////                                    /menu     - show menu
+//////                                    /keyboard - show keyboard";
+//////                    await tbClient.SendTextMessageAsync(message.From.Id, text);
+
+//////                    break;
+
+//////                case "/menu":
+
+//////                    var inLineKeyboard = new InlineKeyboardMarkup(new[]
+//////                    {
+//////                        new[]
+//////                        {
+//////                            InlineKeyboardButton.WithUrl("Google", "https://google.com"),
+//////                            InlineKeyboardButton.WithUrl("Telegram", "https://t.me")
+//////                        },
+//////                        new []
+//////                        {
+//////                            InlineKeyboardButton.WithCallbackData("Item #1"),
+//////                            InlineKeyboardButton.WithCallbackData("Item #2")
+
+//////                        }
+//////                    });
+
+//////                    await tbClient.SendTextMessageAsync(message.From.Id, "Select", replyMarkup: inLineKeyboard);
+
+
+//////                    break;
+//////                case "/keyboard":
+
+//////                    var replyKeyboard = new ReplyKeyboardMarkup(new[]
+//////{
+//////                        new[]
+//////                        {
+//////                            new KeyboardButton("Hi"),
+//////                            new KeyboardButton("How are you?")
+//////                        },
+//////                        new []
+//////                        {
+//////                            new KeyboardButton("Contact") {RequestContact = true},
+//////                            new KeyboardButton("Location") {RequestLocation = true}
+
+//////                        }
+//////                    });
+
+//////                    await tbClient.SendTextMessageAsync(message.Chat.Id, "Message", replyMarkup: replyKeyboard);
+
+//////                    break;
+
+//////                default:
+//////                    break;
+//////            }
+
+
+
+
+//////        }
     }
 }
