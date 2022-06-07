05ba27fc4a8d492ae1c2cefad7b74df2109f16b4
Fedor Zanin <ZaninF@danieli.it>
2022-06-07 16:04:12 +0300

    init: initialize HW for M11

---
+++ Module_11/Homework_11_Task_01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_11_Task_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
