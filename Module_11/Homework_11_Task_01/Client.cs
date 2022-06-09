0f2a8c726bb233216497e72b5486037f3ac84f73
Fedor Zanin <ZaninF@danieli.it>
2022-06-09 12:14:36 +0300

    changes

--- Module_11/Homework_11_Task_01/Client.cs
+++ Module_11/Homework_11_Task_01/Client.cs
@@ -6,50 +6,86 @@ using System.Threading.Tasks;
 
 namespace Homework_11_Task_01
 {
+    /// <summary>
+    /// Client defenition
+    /// </summary>
     public class Client
     {
+        #region Private Fields
         protected string firstName;
         protected string lastName;
         protected string? middleName;
         protected Int64 phoneNumber;
         protected string? passportSerie;
         protected int passportNumber;
+        #endregion
 
+        /// <summary>
+        /// Gets or sets the FirstName
+        /// </summary>
         public string FirstName
         { 
             get { return firstName; } 
             set { firstName = value; } 
         }
 
+        /// <summary>
+        /// Gets or sets the LastName
+        /// </summary>
         public string LastName
         {
             get { return lastName; }
             set { lastName = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the MiddleName
+        /// </summary>
         public string MiddleName
         {
-            get { return middleName; }
+            get { return String.IsNullOrEmpty(middleName) ? "N/A" : middleName; }
             set { middleName = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the PhoneNumber
+        /// </summary>
         public Int64 PhoneNumber
         {
             get { return phoneNumber; }
             set { phoneNumber = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the PassportSerie
+        /// </summary>
         public string PassportSerie
         {
-            get { return passportSerie; }
+            get { return String.IsNullOrEmpty(passportSerie) ? "---" : passportSerie; }
             set
             {
                 passportSerie = value;
             }
-
         }
 
+        /// <summary>
+        /// Gets or sets the PassportNumber
+        /// </summary>
+        public int PassportNumber
+        {
+            get { return passportNumber; }
+            set { passportNumber = value; }
+        }
 
+        /// <summary>
+        /// Client's Constructor
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="middleName"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="passportSerie"></param>
+        /// <param name="passportNumber"></param>
         public Client(string firstName, string lastName, string middleName, Int64 phoneNumber, string passportSerie, int passportNumber)
         {
             this.firstName = firstName;
